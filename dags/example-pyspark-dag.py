5fc87151d364954428323fce9258c3d7a4d35570
hoanb <hoanb@gmail.com>
2024-08-08 13:51:23 +0700

    add plugins

---
+++ dags/example-pyspark-dag.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from plugins.spark_config import Config
+
+DAG_NAME = "dag_spark_operator"
+default_args = {
+    "depends_on_past": False,
+    "start_date": datetime(2023, 1, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    DAG_NAME,
+    default_args=default_args,
+    description="An example DAG with SparkOperator",
+    schedule="*/10 * * * *",
+    catchup=False
+)
+
+
+# Define Python functions to be executed
+def print_hello():
+    return "Hello World!"
+
+
+# Define the PythonOperators
+task1 = PythonOperator(
+    task_id="print_hello",
+    python_callable=print_hello,
+    dag=dag,
+)
+
+spark_submit_task = SparkSubmitOperator(
+    task_id='spark_submit_task',
+    conn_id='spark_default',
+    deploy_mode="cluster",
+    application='s3a://thangdv5/spark/spark-io.py',
+    name='airflow-pyspark-job',
+    verbose=True,
+    conf=Config().config_spark().build(),
+    dag=dag
+)
+
+# Set the task dependencies
+task1 >> spark_submit_task
