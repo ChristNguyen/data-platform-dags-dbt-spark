36f0a14d2ce2948c17e666f40223182e3e36af06
hoanb <hoanb@gmail.com>
2024-08-12 08:01:48 +0700

    update dag

---
+++ dags/example-pyspark-s3-dag.py
@@ -0,0 +1,84 @@
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+class Config:
+    def __init__(self) -> None:
+        self.dict= {}
+        pass
+    def build(self):
+        return self.dict
+    
+    def config_spark(self):
+        self.dict.update({
+            'spark.kubernetes.container.image': 'docker.io/asc686f61/dataplatform_pyspark:v1.2',
+            'spark.executorEnv.LD_PRELOAD':'/opt/bitnami/common/lib/libnss_wrapper.so',
+            'spark.kubernetes.namespace':'spark-operator',
+            'spark.kubernetes.file.upload.path': 's3a://dataplatform/spark',
+            'spark.hadoop.fs.s3a.impl': 'org.apache.hadoop.fs.s3a.S3AFileSystem',
+            'spark.hadoop.fs.s3a.endpoint': 'https://myminio-hl.data-platform-tenant.svc.cluster.local:9000',
+            'spark.hadoop.fs.s3a.fast.upload': 'true',
+            'spark.hadoop.fs.s3a.path.style.access': 'true',
+            'spark.hadoop.fs.s3a.connection.ssl.enabled': 'false',
+            'spark.hadoop.fs.s3a.access.key': 'minio',
+            'spark.hadoop.fs.s3a.secret.key': 'minio123',
+            'spark.jars.ivy': '/tmp',
+            'spark.kubernetes.driver.secrets.kubeconfig':'/secret', \
+            'spark.kubernetes.executor.secrets.kubeconfig':'/secret', \
+            'spark.kubernetes.authenticate.driver.serviceAccountName':'my-release-spark', \
+            'spark.kubernetes.authenticate.executor.serviceAccountName':'my-release-spark', \
+            'spark.driver.extraJavaOptions=-Dcom.sun.net.ssl.checkRevocation':'false', \
+            'spark.executor.extraJavaOptions=-Dcom.sun.net.ssl.checkRevocation':'false', \
+            'spark.ssl.noCertVerification':'true'
+        })
+        return self
+        
+
+
+
+DAG_NAME = "minio_dag_spark_operator"
+default_args = {
+    "depends_on_past": False,
+    "start_date": datetime(2023, 1, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    DAG_NAME,
+    default_args=default_args,
+    description="An example DAG with SparkOperator",
+    schedule=None,
+    catchup=False,
+    max_active_runs=1
+)
+
+
+# Define Python functions to be executed
+def print_hello():
+    return "Hello World!"
+
+
+# Define the PythonOperators
+task1 = PythonOperator(
+    task_id="print_hello",
+    python_callable=print_hello,
+    dag=dag,
+)
+
+spark_submit_task = SparkSubmitOperator(
+    task_id='example_py_spark_pi',
+    conn_id='spark_default',
+    deploy_mode="cluster",
+    application='s3a://dataplatform/spark-jobs/pi.py',
+    name='airflow-pyspark-job',
+    verbose=True,
+    conf=Config().config_spark().build(),
+    dag=dag
+)
+
+# Set the task dependencies
+task1 >> spark_submit_task
